6bed3ac35f6ba6b497e9890e64abcffe845c3e83
SHivam <grademy9@gmail.com>
2019-09-17 19:45:15 +0530

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsComponent
+  ],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+	HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
