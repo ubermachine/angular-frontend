0e0388e87bf48bc7476499727dc0b9a76455e168
UberMachine <shivamkatoch@outlook.com>
2019-09-19 23:45:03 +0530

    added view

--- src - Copy/app/app.module.ts
+++ src - Copy/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { PostsComponent } from './posts/posts.component';
     BrowserModule,
     AppRoutingModule,
 	HttpClientModule,
+	FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
